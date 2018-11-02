7a2199725014682c2da18c29e6c541c664f6c40e
yassernoori <36106744+yassernoori@users.noreply.github.com>
2018-11-02 07:59:49 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -37,7 +37,7 @@ var myTopPostsMenuButton = document.getElementById('menu-my-top-posts');
 function writeNewPost(uid, username, title, body) {
   // A post entry.
   var postData = {
-    author: "Classmate",
+    author: user.email,
     uid: uid,
     body: body,
     title: title,
