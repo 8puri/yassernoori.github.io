92ad663a272f8c0d1fa91ad6a2b4b08f890cb0e8
yassernoori <36106744+yassernoori@users.noreply.github.com>
2018-11-03 13:25:39 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -96,7 +96,7 @@ function createPostElement(postId, title, text, author) {
               '<div class="username mdl-color-text--white"></div>' +
             '</div>' +
           '</div>' +
-          '<span class="star">' +
+          '<span style="color:white" class="star">' +
             '<div class="not-starred material-icons">star_border</div>' +
             '<div class="starred material-icons">star</div>' +
             '<div class="star-count">0</div>' +
