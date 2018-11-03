806635e021803c6aa8e83002054aad80527aba7d
yassernoori <36106744+yassernoori@users.noreply.github.com>
2018-11-03 16:58:33 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+alert("Welcome to the class website! \n\n Updates: \n - Added login \n - Added date \n - Fixed bugs ");
+
 'use strict';
 
 // Shortcuts to DOM Elements.
