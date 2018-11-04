092c5c56557a028365ead089123ceba61afdb508
yassernoori <36106744+yassernoori@users.noreply.github.com>
2018-11-04 16:47:47 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -56,7 +56,7 @@ var dr = d.toDateString();
 
   // A post entry.
   var postData = {
-    author: y + " Date: " + dr,
+    author: y + " \n Date: " + dr,
     uid: uid,
     body: body,
     title: title,
